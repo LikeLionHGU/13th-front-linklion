46c64ca293c396fad050704ade62c1fbb12243d3
lingardd09 <sherlockicker@gmail.com>
2025-01-22 02:01:45 +0900

    원진 - 둘러보기 페이지 프로토타입 완성

--- src/routes/List.js
+++ src/routes/List.js
@@ -1,19 +1,28 @@
 import { useEffect, useState } from "react";
-import detial_appbar from "../assets/grid/detail_appbar.png";
-import create_button from "../assets/grid/create_button.png"
-
+import detail_appbar from "../assets/grid/detail_appbar.png";
+import create_button from "../assets/grid/create_button.png";
+import Card from "../components/Card";
+import "../styles/List.css"
 
 function List() {
   return (
-    <div>
-      <header className="header">
-        <div className="headerImg">
-          <img src={detial_appbar} alt="detail-appbar"></img>
-          <img src={create_button} alt="create-button"></img>
+    <div id="main">
+        <div id="detail-appbar-box">
+          <img id="detail-appbar" src={detail_appbar} alt="detail-appbar"></img>
         </div>
-        <img className="headerLogo"></img>
-        <img className="headerLogin"></img>
-      </header>
+        <div id="create-button-box">
+          <img id="create-button" src={create_button} alt="create-button"></img>
+        </div>
+        <div id="card-list">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+
+        </div>
     </div>
   );
 }
