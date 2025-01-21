47c7f97f37e7cff19dff002d1de219cecb006b30
junnyang <lhr8440@gmail.com>
2025-01-22 01:46:26 +0900

    app.js browserrouter 추가

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Main from "./routes/Main";
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/main">
-          <Main />
-        </Route>
-      </Switch>
-    </Router>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Router>
+        <Switch>
+          <Route path="/main">
+            <Main />
+          </Route>
+        </Switch>
+      </Router>
+    </BrowserRouter>
   );
 }
 
