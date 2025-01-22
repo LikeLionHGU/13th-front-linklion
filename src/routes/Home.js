bfb282aa82eafa0fce35f3ac849d470c8baf2641
lingardd09 <sherlockicker@gmail.com>
2025-01-23 03:00:07 +0900

    둘러보기 페이지 알파 버전 완성

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,31 +1,6 @@
-import { useEffect, useState } from "react";
-import background from "../assets/Main_BackGround.png";
-import logo from "../assets/Logo(Icon).png";
-import "../css/Main.css";
-import Appbar from "../assets/Appbar.png";
 
 function Home() {
-  return (
-    <div>
-      <div className="body">
-        <div className="background">
-          <img src={background} alt="background" className="background"></img>
-          <header className="header">
-            <div className="headerImg">
-              <img src={Appbar} alt="header" className="headerImg"></img>
-            </div>
-            <div className="headerLogo">
-              <button className="headerLogo"> </button>
-              <img src={logo} className="headerLogo"></img>
-            </div>
-
-            <button className="headerLogin">로그인</button>
-          </header>
-          <div></div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div>Home</div>;
 }
 
 export default Home;
