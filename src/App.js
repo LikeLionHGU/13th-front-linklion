ad7d04014c24bbb35e531dda16cc4a4804ecfd71
lingardd09 <sherlockicker@gmail.com>
2025-01-22 21:42:53 +0900

    원진 - 둘러보기 페이지 프로토타입 2차 완성

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Main from "./routes/Main";
 import List from "./routes/List";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/Main">
-          <Main />
-        </Route>
         <Route path="/List">
           <List />
         </Route>
